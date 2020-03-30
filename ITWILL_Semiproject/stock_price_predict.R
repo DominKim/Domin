0a1cc9d883323ce6bb4a6f525806e13f8d9ca19a
domin <hyungm3@gmail.com>
2020-03-30 16:10:55 +0900

    세미 수정

--- ITWILL_Semiproject/stock_price_predict.R
+++ ITWILL_Semiproject/stock_price_predict.R
@@ -14,8 +14,8 @@ cor_re <- vector(mode = "list", length = 3)
 for (i in names) {
   cnt <- cnt + 1
   cat("y 와", i, "의 상관계수는")
-  mods[[cnt]] <- lm(y ~ iris2[,i], data = iris2)
+  mods[[cnt]] <- lm(Sepal.Length ~ iris2[,i], data = iris2)
   pred[[cnt]] <- predict(mods[[cnt]], iris2)
-  cor_re[[cnt]] <- cor(iris2$y, iris2[,i])
+  cor_re[[cnt]] <- cor(iris2$Sepal.Length, iris2[,i])
   cat(cor_re[[cnt]], "이다.", "\n")
 }
